944bec89593e23e865383ba8e96f9cd27cca2d1f
farazzhh <havaee.faraz@gmail.com>
2021-10-14 00:11:22 +0330

    looking for Vercel build issue

--- src/components/dataFetch/DataFetch.jsx
+++ src/components/dataFetch/DataFetch.jsx
@@ -1,13 +1,16 @@
 const DataFetch = async (city) => {
-  console.log("datafetch");
   const API_KEY = "6a710700f50ffccf3922b4d1c7eeb555";
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+  let result = undefined;
+  if (city) {
+    result = await fetch(URL)
+      .then((response) => response.json())
+      .then((data) => data)
+    return result;
+  } else {
+    result = { cod: "400" };
+  }
 
-  const result = await fetch(URL)
-    .then((response) => response.json())
-    .then((data) => data);
-
-  console.log(result);
   return result;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "React";
-import  ReactDOM  from "React-Dom";
+import React from "react";
+import  ReactDOM  from "react-dom";
 
 import App from './App';
 
